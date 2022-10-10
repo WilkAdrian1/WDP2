c1ac3f7b585de177fe22832079bd3333b8b95007
WilkAdrian1 <74956694+WilkAdrian1@users.noreply.github.com>
2022-10-10 11:12:39 +0200

    Add files via upload

---
+++ WDP5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n WDP jest najlepszy!!!";
+
+    int Maciek;
+
+    Maciek = 20 * 2;
+
+    cout << '\n' << Maciek;
+
+    return 1;
+}
